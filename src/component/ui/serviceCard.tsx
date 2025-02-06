3fd443773fc9e02154324dc6b50ec0645b4e98a2
PreyeO <omusukup@yahoo.com>
2025-02-06 15:04:10 +0100

    changes

--- src/component/ui/serviceCard.tsx
+++ src/component/ui/serviceCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ServiceCard = ({ title, description, className }) => {
   return (
     <div
-      className={`${className} md:w-[220px] 2xl:w-[300px] 2xl:h-[350px] w-[160px] card h-[200px]  shadow-md md:p-4 2xl:p-6 text-center rounded-lg mx-auto flex flex-col justify-center gap-5`}
+      className={`${className} md:w-[220px] 2xl:min-w-[300px] 2xl:h-[350px] w-[160px] card h-[200px]  shadow-md md:p-4 2xl:p-6 text-center rounded-lg mx-auto flex flex-col justify-center gap-5`}
     >
       <h2 className=" font-bold md:text-lg 2xl:text-2xl text-base md:leading-7">
         {title}
