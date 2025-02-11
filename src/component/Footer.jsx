e9c2f60ff610e25d733352c59a1f3d1f286776b5
PreyeO <omusukup@yahoo.com>
2025-02-11 16:08:39 +0100

    CHANGES

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -21,7 +21,9 @@ const Footer = () => {
               <h5 className="font-bold text-lg 2xl:text-3xl text-[#1d2951]">
                 Office Address:
               </h5>
-              <p>Lagos: No 3, Femi Okunnu Estate Phase 1, Lekki Lagos</p>
+              <p>
+                Lagos: THE CHALET, No 3, Femi Okunnu Estate Phase 1, Lekki Lagos
+              </p>
             </div>
             <div className="md:w-[200px] 2xl:w-[300px] w-full flex flex-col gap-2 text-sm 2xl:text-xl">
               <h5 className="font-bold text-lg text-[#1d2951] 2xl:text-3xl ">
